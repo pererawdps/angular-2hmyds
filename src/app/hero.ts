bd07b613b2e89e425cb085fef011932ef5a100a9
pererawdps <pererawdps@gmail.com>
2021-09-08 09:20:44 +0530

    Initial commit

---
+++ src/app/hero.ts
@@ -0,0 +1,18 @@
+export interface Hero {
+  id: number;
+  name: string;
+}
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
